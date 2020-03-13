dc8996c3ee73175547ee028bcaeaad80566deb6d
Jamal McDowell <jamal.mcdowell@gmail.com>
2020-03-13 19:42:10 -0400

    Added baseURL, added hidden into state for future terary. Setup initial pull from both apis.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,44 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+let baseURL = ''
+if (process.env.NODE_ENV === 'development') {
+  baseURL = 'http://localhost:3003'
 }
+
+  class App extends React.Component {
+    constructor(props){
+      super(props)
+      this.state = {
+        amiibos: [],
+        isHidden: false
+      }
+    }
+
+    componentDidMount(){
+      this.getAmiibos()
+    }
+
+    async getAmiibos() {
+      try {
+        // SETTING THE ARRAY WE ARE PERMUTING TO
+        let completeData = []
+        // INITIAL DATABASE PULL OF THE CUSTOM AMIIBOS
+        let response = await fetch(`${baseURL}/amiibos`)
+        let data = await response.json()
+        // PULLING FROM EXTERNAL API
+        let amiiboExternalApi = await fetch(`https://www.amiiboapi.com/api/amiibo/`)
+        let externalData = await amiiboExternalApi.json()
+        completeData = [data, ...externalData.amiibo]
+        this.setState({amiibos: completeData})
+        console.log(completeData);
+      } catch (err) {
+        console.log(err);
+      }
+    }
+    render(){
+      return(
+        <h1>Welcome to Amiibo Wishlist!</h1>
+      )
+    }
+  }
 
 export default App;
